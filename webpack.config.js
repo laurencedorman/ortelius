b3081b153d433f05c47440511f2f7acb8bef8ac1
Laurence Dorman <hello@laurencedorman.com>
2019-02-05 21:47:16 +0100

    basic setup

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/editor.js",
+  mode: "development",
+  output: {
+    filename: "./main.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+}
