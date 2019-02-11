18221c2db677c7aa4f5252d1b4e2a40cc57d82b0
Laurence Dorman <hello@laurencedorman.com>
2019-02-11 07:34:18 +0100

    on track for v0

--- src/editor.jsx
+++ src/editor.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(
-  <h1>Hello, there!</h1>,
-  document.getElementById('root'),
-);
+import {
+  geoPath,
+  geoMercator,
+} from 'd3-geo';
+
+const rawWidth = document.documentElement.clientWidth;
+const rawHeight = document.documentElement.clientHeight;
+
+const Path = ({ path, id }) => {
+  const alertId = (id) => {
+    alert(id);
+  };
+
+  return (
+    <path onClick={alertId.bind(null, id)} d={path} />
+  );
+};
+
+const Map = ({ geojson, projection, height, width }) => {
+  const path = geoPath().projection(projection.fitExtent([
+    [0, 0],
+    [width, height],
+  ], geojson));
+
+  const renderPath = datum => (
+    <Path
+      key={datum.properties.code}
+      id={datum.properties.code}
+      path={path(datum)}
+    />
+  );
+
+  return (
+    <div>
+      <svg width={width} height={height}>
+        <g transform="translate(0,0)">
+          {geojson.features.map(renderPath)}
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+fetch('./france-departements-simplified.geojson')
+  .then(res => res.json())
+  .then((geojson) => {
+    render(
+      <Map
+        geojson={geojson}
+        projection={geoMercator()}
+        height={rawHeight}
+        width={rawWidth}
+      />,
+      document.getElementById('root'),
+    );
+  });
