336aca3b3482451d4d4d581a89e1e1cc42f9ff2e
Laurence DORMAN <laurence.dorman@manomano.com>
2019-02-12 13:58:38 +0100

    horrific wip

--- src/viewer.jsx
+++ src/viewer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { useSpring, animated, config } from 'react-spring';
 
@@ -6,6 +6,8 @@ import {
   geoPath,
   geoMercator,
 } from 'd3-geo';
+
+// https://stackoverflow.com/questions/2916081/zoom-in-on-a-point-using-scale-and-translate
 
 const rawWidth = document.documentElement.clientWidth;
 const rawHeight = document.documentElement.clientHeight;
@@ -17,8 +19,21 @@ const Path = ({ path, id }) => {
 };
 
 const Map = ({ geojson, projection,height, width}) => {
+  const [count, setCount] = useState(0);
+
+  const [props, set] = useSpring(() => ({
+    transform: 'translate(0,0) scale(0.9)',
+    from: {
+      transform: 'translate(0,0) scale(1)',
+    },
+    config: config.default,
+  }))
+  
+  set({ opacity: toggle ? 1 : 0 });
+  set({ transform: toggle ? 'translate(0,0) scale(0.9)' });
+
   const props = useSpring({
-    transform: 'translate(0,0) scale(0.2)',
+    transform: 'translate(0,0) scale(0.9)',
     from: {
       transform: 'translate(0,0) scale(1)',
     },
@@ -37,12 +52,25 @@ const Map = ({ geojson, projection,height, width}) => {
       path={path(datum)}
     />
   );
-  console.log(props.scale);
+  
+  // console.log(props.scale);
+
+  const zoomIn = () => {
+    debugger;
+    setCount(count * 0.9);
+  };
+
+  const zoomOut = () => {
+
+  };
+
+  const { transform } = props;
+
   return (
     <div>
       <svg
         width={width}
-        height={height}
+        height={height - 40}
         // onMouseDown={this.onDragStart.bind(this)}
         // onTouchStart={this.onDragStart.bind(this)}
         // onMouseMove={this.onDragMove.bind(this)}
@@ -50,14 +78,15 @@ const Map = ({ geojson, projection,height, width}) => {
         // onMouseUp={this.onDragEnd.bind(this)}
         // onWheel={this.onWheel.bind(this)}
       >
-        <animated.g transform={props.transform}>
+        <animated.g transform={transform}>
           {geojson.features.map(renderPath)}
         </animated.g>
       </svg>
+      <button onClick={zoomIn.bind(this)}>Zoom In</button>
+      {/* <button onClick={zoomOut.bind(this)}>Zoom out</button> */}
     </div>
   );
 };
-
 
 fetch('./france-departements-simplified.geojson')
   .then(res => res.json())
