aec482b38fe4e98d50cbd495d11281641045f811
Laurence Dorman <hello@laurencedorman.com>
2019-02-25 21:59:07 +0100

    bounding box zoom

--- src/components/GeoFeature.jsx
+++ src/components/GeoFeature.jsx
@@ -1,47 +1,10 @@
 import React, { useState } from 'react';
 import { useSpring, animated, config, interpolate } from 'react-spring';
 
-const GeoFeature = ({ bounds, path }) => {
-  let pathElem = null;
-  
-  const [props, set] = useSpring(() => ({
-    fill: 'black',
-    scale: 1,
-  }));
-
-  const onMouseOver = () => {
-    console.log(bounds);
-    debugger;
-
-    var dx = bounds[1][0] - bounds[0][0];
-    var dy = bounds[1][1] - bounds[0][1];
-    var x = (bounds[0][0] + bounds[1][0]) / 2;
-    var y = (bounds[0][1] + bounds[1][1]) / 2;
-    var scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height)));
-    var translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-    set({
-      fill: 'yellow',
-      scale: 1.1,
-    });
-
-    setTimeout(() => {
-      set({
-        fill: 'black',
-        scale: 1,
-      });
-    }, 300);
-  };
-
-  const { fill, scale } = props;
-
+const GeoFeature = ({ bounds, path, onClick }) => {
   return (
     <animated.path
-      ref={(path) => { pathElem = path; }} 
-      fill={fill}
-      transform={scale.interpolate(x => `scale(${x})`)}
-      onFocus={onMouseOver}
-      onMouseOver={onMouseOver}
+      onClick={onClick.bind(null, bounds)}
       d={path}
     />
   );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,10 +9,9 @@ import {
 import GeoFeature from './GeoFeature';
 
 const Map = ({ geojson, projection = geoMercator(), height, width}) => {
-  const animProps = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-  });
+  const [{ transform }, set] = useSpring(() => ({
+    transform: [0, 0, 1],
+  }));
 
   const path = geoPath().projection(projection.fitExtent([
     [0, 0],
@@ -24,15 +23,38 @@ const Map = ({ geojson, projection = geoMercator(), height, width}) => {
     bounds: path.bounds(feature),
   }));
 
+  const onClick = (bounds) => {
+    const dx = bounds[1][0] - bounds[0][0];
+    const dy = bounds[1][1] - bounds[0][1];
+    const x = (bounds[0][0] + bounds[1][0]) / 2;
+    const y = (bounds[0][1] + bounds[1][1]) / 2;
+    const scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height)));
+    const translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+    set({
+      transform: [...translate, scale],
+    });
+
+    setTimeout(() => {
+      set({
+        transform: [0, 0, 1],
+      });
+    }, 3000)
+  };
+
   return (
     <div>
       <svg
         width={width}
         height={height - 40}
       >
-        <animated.g opacity={animProps.opacity}>
+        <animated.g 
+          transform={
+            transform.interpolate((x, y, s) => (`translate(${x},${y}) scale(${s})`))
+          }
+        >
           {paths.map(({ bounds, d }) => (
-            <GeoFeature bounds={bounds} key={d} path={d} />
+            <GeoFeature bounds={bounds} onClick={onClick} key={d} path={d} />
           ))}
         </animated.g>
       </svg>
